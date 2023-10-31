ceb61da76e8188cc348fb78286f96fe58aa2da14
Lucas Gabriel Tamanini Demori <lucas-demori2001@hotmail.com>
2023-10-30 21:47:12 -0300

    Crud-laravel

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
 use Illuminate\Http\Request;
 
@@ -19,13 +20,51 @@ class SupportController extends Controller
         return view('admin/supports/create');
     }
 
-    public function store(Request $request, Support $support)
+
+    public function store(StoreUpdateSupport $request, Support $support)
     {
         $data = $request->all();
         $data['status'] = 'a';
         
         $support = $support->create($data);
 
+        return redirect()->route('supports.index');
+    }
+
+    public function show(string|int $id)
+    {
+        if(!$support = Support::find($id)){
+            return back();
+        }
+        return view('admin/supports/show', compact('support'));
+    }
+
+    public function edit(Support $support, string|int $id)
+    {
+        if(!$support = $support->where('id', $id)->first()){
+            return back();
+        }
+        return view('admin/supports.edit', compact('support'));
+    }
+
+    public function update(Request $request, Support $support, string $id)
+    {
+        if(!$support = $support->find($id)){
+            return back();
+        }
+
+        $support->update($request->only([
+            'subject', 'body'
+        ]));
+        return redirect()->route('supports.index');
+
+    }
+
+    public function destroy(string|int $id, Support $support)
+    {
+        if(!$support = $support->find($id)->delete()){
+            return back();
+        }
         return redirect()->route('supports.index');
     }
 }
--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -13,7 +13,10 @@
                 <td>{{$support->subject}}</td>
                 <td>{{$support->status}}</td>
                 <td>{{$support->body}}</td>
-                <td>></td>
+                <td>
+                    <a href="{{ route('supports.show', $support->id) }}">Ir</a>
+                    <a href="{{ route('supports.edit', $support->id) }}">Editar</a>
+                </td>
             </tr>
             @endforeach
         </tbody>
