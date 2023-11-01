717e7ea778516db64877f95ce2acdbd55793f30c
Lucas Gabriel Tamanini Demori <lucas-demori2001@hotmail.com>
2023-11-01 09:29:21 -0300

    Create repository and DTO's

--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,7 +1,12 @@
 <h1>Dúvida {{ $support->id }}</h1>
 
+@if($errors->any())
+    @foreach($errors->all() as $error)
+        {{ $error }}
+    @endforeach
+@endif
+
 <form action="{{ route('supports.update', $support->id) }}" method="POST">
-    <!-- <input type="text" value="{{ csrf_token() }}" name="_token"> -->
     @csrf()
     @method('PUT')
     <input type="text" placeholder="Assunto" name="subject" value="{{$support->subject}}">
